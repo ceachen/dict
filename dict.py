32166ff1a7d0fff8d41dece95bc153f919b9e5f2
cichen.wang <cichen.wang@hobot.cc>
2018-05-21 18:03:18 +0800

    add query local db. step 0.1

--- dict.py
+++ dict.py
@@ -2,15 +2,20 @@
 
 import sys
 import json
-import urllib
-import urllib2
-
+import requests
+import pymysql
 
 """
 dict - Chinese/English Translation
 @author Feei(wufeifei@wufeifei.com)
 @date   2013.12.09
 """
+dictDb = {"host": "127.0.0.1",
+          "port": "3306",
+          "user": "root",
+          "passwd": "Hobot123456!",
+          "db": "english_dict",
+          "charset": "utf8"}
 
 
 class Dict:
@@ -18,28 +23,41 @@ class Dict:
     keyFrom = 'wufeifei'
     api = 'http://fanyi.youdao.com/openapi.do?keyfrom=wufeifei&key=716426270&type=data&doctype=json&version=1.1&q='
     content = None
+    askWord = ""
 
     def __init__(self, argv):
         if len(argv) == 1:
-            self.api = self.api + urllib.quote(argv[0])
-            self.translate()
+            self.askWord = argv[0]
+            if self.askWord.isalpha():
+                self.en2cn_flag = True
+            self.conn = pymysql.connect(host=dictDb["host"], port=dictDb["port"], user=dictDb["user"], passwd=dictDb["passwd"],
+                                   db=dictDb["db"], charset=dictDb["charset"], unix_socket="/var/lib/mysql/mysql.sock")
+            self.cur = self.conn.cursor()
+            self.myFind()
+            self.release()
         else:
-            print 'ERROR'
+            print("ERROR. usage: dict word")
 
-    def translate(self):
-        content = urllib2.urlopen(self.api).read()
-        self.content = json.loads(content)
+    def myFind(self):
+        if not self.local_find():
+            self.api = self.api + self.askWord
+
+            content = requests.get(self.api).text
+            self.content = json.loads(content)
+            self.update_local()
+
         self.parse()
 
     def parse(self):
         code = self.content['errorCode']
+
         if code == 0:  # Success
             try:
-                u = self.content['basic']['us-phonetic'] # English
+                u = self.content['basic']['us-phonetic']  # English
                 e = self.content['basic']['uk-phonetic']
             except KeyError:
                 try:
-                    c = self.content['basic']['phonetic'] # Chinese
+                    c = self.content['basic']['phonetic']  # Chinese
                 except KeyError:
                     c = 'None'
                 u = 'None'
@@ -50,36 +68,65 @@ class Dict:
             except KeyError:
                 explains = 'None'
 
-            print '\033[1;31m################################### \033[0m'
-            print '\033[1;31m# \033[0m', self.content['query'], self.content['translation'][0],
+            print('- '*40)
+            print(" %s, %s" % (self.content['query'], self.content['translation'][0]))
             if u != 'None':
-                print '(U:', u, 'E:', e, ')'
+                print('(U:', u, 'E:', e, ')')
             elif c != 'None':
-                print '(Pinyin:', c, ')'
+                print('(Pinyin:', c, ')')
             else:
-                print
+                print()
 
             if explains != 'None':
                 for i in range(0, len(explains)):
-                    print '\033[1;31m# \033[0m', explains[i]
+                    print('', explains[i])
             else:
-                print '\033[1;31m# \033[0m Explains None'
-            print '\033[1;31m################################### \033[0m'
-            # Phrase
-            # for i in range(0, len(self.content['web'])):
-            #     print self.content['web'][i]['key'], ':'
-            #     for j in range(0, len(self.content['web'][i]['value'])):
-            #         print self.content['web'][i]['value'][j]
+                print('Explains None')
+            print('- '*40)
+
         elif code == 20:  # Text to long
-            print 'WORD TO LONG'
+            print('WORD TO LONG')
         elif code == 30:  # Trans error
-            print 'TRANSLATE ERROR'
+            print('TRANSLATE ERROR')
         elif code == 40:  # Don't support this language
-            print 'CAN\'T SUPPORT THIS LANGUAGE'
+            print('CAN\'T SUPPORT THIS LANGUAGE')
         elif code == 50:  # Key failed
-            print 'KEY FAILED'
+            print('KEY FAILED')
         elif code == 60:  # Don't have this word
-            print 'DO\'T HAVE THIS WORD'
+            print('DO\'T HAVE THIS WORD')
+
+
+    def local_find(self):
+
+        if self.en2cn_flag:
+            sqlCmd = "SELECT * FROM en2cn_tbl WHERE word = \"" + self.askWord + "\""
+        else:
+            sqlCmd = "SELECT * FROM cn2en_tbl WHERE word = \"" + self.askWord + "\""
+
+        self.cur.execute(sqlCmd)
+        # print(self.cur.fetchone())
+        if self.cur.rowcount == 0:
+            # //cur.close()
+            # //conn.close()
+            return False
+        self.content = self.cur.fetchone()
+        return True
+
+    def update_local(self):
+        if self.en2cn_flag:
+            # sqlCmd = "INSERT INTO en2cn_tbl(word, explain) VALUES(\"" + self.askWord + "\", \"" + str(self.content) + "\""
+            pass
+        else:
+            pass
+            # sqlCmd = "INSERT INTO cn2en_tbl(word, explain) VALUES(\"" + self.askWord + "\", \"" + str(self.content) + "\""
+
+        # self.cur.execute(sqlCmd)
+        # self.conn.commit()
+
+    def release(self):
+        self.cur.close()
+        self.conn.close()
+
 
 if __name__ == '__main__':
     Dict(sys.argv[1:])
